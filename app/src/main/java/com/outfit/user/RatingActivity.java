1b640a817ae4a47edde69c0db74e6a013d7113f0
inder <inderjitbassi31@gmail.com>
2020-09-28 15:45:43 -0400

    cart interfaces with seller profile data  and rating activity

---
+++ app/src/main/java/com/outfit/user/RatingActivity.java
@@ -0,0 +1,42 @@
+package com.outfit.user;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RatingActivity extends AppCompatActivity {
+
+    ImageView back;
+    Button btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+
+        back = findViewById(R.id.back);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RatingActivity.this, HomeActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
