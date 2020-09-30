15961bc7c539c4cee9bb7590d8a42a56bdddec67
davinder <damanthakur1610@gmail.com>
2020-09-29 22:43:14 -0400

    Home interface with links to other interfaces using navigation drawer activity and seller activity interfaces

--- app/src/main/java/com/outfit/user/seller/SellerRegisterActivity.java
+++ app/src/main/java/com/outfit/user/seller/SellerRegisterActivity.java
@@ -1,6 +1,10 @@
 package com.outfit.user.seller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,9 +13,33 @@ import com.outfit.user.R;
 
 public class SellerRegisterActivity extends AppCompatActivity {
 
+
+    ImageView back;
+    Button btnSubmit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_register);
+
+        back = findViewById(R.id.back);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SellerRegisterActivity.this, SellerProfileActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
